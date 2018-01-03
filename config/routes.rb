298b3613c14a9ea94a0b241c25be682ae7952f25
Hakuto Fujimori <hakuto_nakase@yahoo.co.jp>
2018-01-03 12:03:13 +0000

    fix link

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'calendar/index',to: "calendar#index"
+  get 'calendar/all', to: 'calendar#all'
+  get 'calendar/index',to: 'calendar#index'
   devise_for :samples
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'index#index'
